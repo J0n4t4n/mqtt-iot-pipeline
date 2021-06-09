1ff9834f2296739d36d24565f6310256d79abd39
Jonatan Steuernagel <jonatan@jsteuernagel.de>
2021-06-09 14:35:49 +0200

    Create bare server

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/J0n4t4n/mqtt-iot-pipeline
+
+go 1.16
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.3.5 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func main() {
+	co := mqtt.NewClientOptions()
+	co.AddBroker("tcp://ezmeral52gateway1.demo.local:10003")
+
+	client := mqtt.NewClient(co)
+	<-client.Connect().Done()
+
+	storage := map[string]bool{}
+
+	<-client.Subscribe("sensors/#", 0, func(cl mqtt.Client, me mqtt.Message) {
+		messageHandler(cl, me, storage)
+	}).Done()
+
+	http.HandleFunc("/availability", func(w http.ResponseWriter, r *http.Request) {
+		availabilityHandler(w, r, storage)
+	})
+
+	http.ListenAndServe(":8080", nil)
+
+	log.Println(storage)
+}
+
+func messageHandler(cl mqtt.Client, me mqtt.Message, storage map[string]bool) {
+	s := strings.Split(me.Topic(), "/")
+	s = s[1:]
+
+	log.Println(s[0])
+	log.Println(string(me.Payload()))
+
+	productAvailable, _ := strconv.Atoi(string(me.Payload()))
+
+	storage[s[0]] = productAvailable > 5000
+}
+
+func availabilityHandler(w http.ResponseWriter, r *http.Request, storage map[string]bool) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	query := r.URL.Query()
+	productId, present := query["productId"]
+	if !present || len(productId) == 0 {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	w.Write([]byte(strconv.FormatBool(storage[productId[0]])))
+}
